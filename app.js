907b4ea7150ad7ff78a636affd5f7e55a86873db
777ernest888-oss <777ernest888@gmail.com>
2026-05-24 00:43:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ async function init() {
     }
 
     // Загрузка данных (пример структуры, адаптируй под свою таблицу)
-    const sheetUrl = 'https://docs.google.com/spreadsheets/d/ТВОЙ_ID/pub?output=csv';
+    const sheetUrl = 'https://docs.google.com/spreadsheets/d/1tLCnDY0j9GNpVde3P9XF9VVjpi2xLGXy_3ScYxEYSXk/pub?output=csv';
     listings = await loadFromGoogleSheets(sheetUrl);
    
     renderFilters();
