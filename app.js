8ddf0bfb7a8ee80c38a6fef54957a87a4f9674ed
777ernest888-oss <777ernest888@gmail.com>
2026-05-24 09:41:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 console.log("🚀 ГАБ Калькулятор запущен!");
 
 // 🎥 НАСТРОЙКИ ВИДЕО - вставь ссылку или оставь пустой
-const WELCOME_VIDEO = ''; // Пример: 'https://.../video.mp4'
+// ВАЖНО: Ссылка должна начинаться с raw.githubusercontent.com, иначе не сработает
+const WELCOME_VIDEO = 'https://raw.githubusercontent.com/777ernest888-oss/newgab/main/welcome.mp4';
 
 const SHEET_ID = '1tLCnDY0j9GNpVde3P9XF9VVjpi2xLGXy_3ScYxEYSXk';
 const SHEET_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/export?format=csv&gid=0`;
